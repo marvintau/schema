e8da926bf34b5ef00fe9b228ab502834d18d8324
陶 <nightvista@gmail.com>
2020-05-27 18:41:15 +0800

    yeah

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const trav = (data, schema, func) => {
+
+  if(schema === undefined){
+    throw TypeError('Schema should be provided.');
+  }
+
   if (typeof schema === 'string'){
 
-    return func({type: 'prim', data, schema});
+    return func({type:'prim', data, schema});
   
   } else if (Array.isArray(schema)){
 
@@ -13,7 +18,8 @@ const trav = (data, schema, func) => {
       throw TypeError(`${data} is not array`);
     }
 
-    return data.map((elem) => trav(elem, schema[0], func));
+    const result = data.map((elem) => trav(elem, schema[0], func));
+    return func({type:'list', data: result, schema});
 
   } else if (schema.constructor === Object) {
 
@@ -24,96 +30,136 @@ const trav = (data, schema, func) => {
     const entries = Object.entries(schema)
           .map(([key, value]) => [key, trav(data[key], value, func)]);
     
-    return Object.fromEntries(entries);
+    const result = Object.fromEntries(entries);
+
+    return func({type:'dict', data: result, schema});
   }
 }
-
 
 const validate = (data, schema) => {
 
-  if (schema === undefined){
-    throw TypeError('You must provide type for validation')
-  }
+  const validFunc = ({type, data, schema}) => {
 
-  const trace = {data, schema};
+    const prim = (data, schema) => {
+      const ok = typeof data === schema;
+      const trace = {data, schema};
 
-  if (typeof schema === 'string'){
-
-    // for primitive type, if the data is undefined, e.g. the provided
-    // data doesn't contain some keys provided by type.
-
-    const type = 'prim';
-
-    const ok = typeof data === schema;
-    
-    return ok ? { ok, type } : { ok, trace, type};
-  
-  } else if (Array.isArray(schema) && schema.length === 1){
-
-    const type = 'list';
-    
-    if (!Array.isArray(data)){
-
-      const ok = false;
-      return {ok, trace, type}
-
-    } else {
-
-      const subs = data.map((innerElem) => validate(innerElem, schema[0]));
-      const ok = subs.every(({ok}) => ok);
-      const trace = subs.filter(({ok}) => !ok);
-  
-      return ok ? {ok, type} : { ok, trace, type};
+      return ok ? {ok} : {ok, trace};
     }
 
-  } else if (schema.constructor === Object){
-
-    const type = 'dict';
-
-    if (data === undefined){
-      const ok = false;
-      const error = 'mismatch';
-      return {ok, trace, error, suggest, type};
+    const list = (data) => {
+      const entries = data.entries();
+      const ok = data.every(([, {ok}]) => ok);
+      const [index, trace] = entries.find(([, {ok}]) => !ok);
+  
+      return ok ? {ok} : {ok, trace:{[index]: trace}};
     }
 
-    const subEntries = Object.entries(schema).map(([k, innerType]) => {
-      return [k, validate(data[k], innerType)]
-    });
-    const ok = subEntries.every(([,{ok}]) => ok);
-    const trace = Object.fromEntries(subEntries.filter(([, {ok}]) => !ok));
-    
-    const error = !ok ? 'inner': 'mismatch';
 
-    return {ok, error, trace, type};
+    const dict = (data) => {
+      const entries = Object.entries(data);
+      const ok = entries.every(([, {ok}]) => ok);
+      const [key, trace] = data.find(([, {ok}]) => !ok);
+
+      return ok ? {ok} : {ok, trace:{[key]: trace}};
+    }
+
+    const funcs = { prim, list, dict }
+
+    return (type in funcs) ? funcs[type](data, schema) : {ok: false};
   }
 
-  return {ok: false, trace, type: 'unknown'}
+  return trav(data, schema, validFunc);
 }
 
-class Schema {
-  constructor(schema){
-    if (schema === undefined){
-      throw TypeError('schema cannot be undefined.');
-    }
+// const validate = (data, schema) => {
 
-    this.schema = schema;
-  }
+//   if (schema === undefined){
+//     throw TypeError('You must provide type for validation')
+//   }
 
-  validate(data){
-    return validate(data, this.schema);
-  }
+//   const trace = {data, schema};
 
-  create(data){
+//   if (typeof schema === 'string'){
+
+//     // for primitive type, if the data is undefined, e.g. the provided
+//     // data doesn't contain some keys provided by type.
+
+//     const type = 'prim';
+
+//     const ok = typeof data === schema;
     
-    const {ok, trace} = validate(data, this.schema);
-    
-    if (ok){
-      return data;
-    } else {
-      console.log(trace, 'error');
-      throw TypeError(`data does not conform to given schema`)
-    }
-  }
-}
+//     return ok ? { ok, type } : { ok, trace, type};
+  
+//   } else if (Array.isArray(schema) && schema.length === 1){
 
-module.exports = Schema;
+//     const type = 'list';
+    
+//     if (!Array.isArray(data)){
+
+//       const ok = false;
+//       return {ok, trace, type}
+
+//     } else {
+
+//       const subs = data.map((innerElem) => validate(innerElem, schema[0]));
+//       const ok = subs.every(({ok}) => ok);
+//       const trace = subs.filter(({ok}) => !ok);
+  
+//       return ok ? {ok, type} : { ok, trace, type};
+//     }
+
+//   } else if (schema.constructor === Object){
+
+//     const type = 'dict';
+
+//     if (data === undefined){
+//       const ok = false;
+//       const error = 'mismatch';
+//       return {ok, trace, error, suggest, type};
+//     }
+
+//     const subEntries = Object.entries(schema).map(([k, innerType]) => {
+//       return [k, validate(data[k], innerType)]
+//     });
+//     const ok = subEntries.every(([,{ok}]) => ok);
+//     const trace = Object.fromEntries(subEntries.filter(([, {ok}]) => !ok));
+    
+//     const error = !ok ? 'inner': 'mismatch';
+
+//     return {ok, error, trace, type};
+//   }
+
+//   return {ok: false, trace, type: 'unknown'}
+// }
+
+// class Schema {
+//   constructor(schema){
+//     if (schema === undefined){
+//       throw TypeError('schema cannot be undefined.');
+//     }
+
+//     this.schema = schema;
+//   }
+
+//   validate(data){
+//     return validate(data, this.schema);
+//   }
+
+//   create(data){
+    
+//     const {ok, trace} = validate(data, this.schema);
+    
+//     if (ok){
+//       return data;
+//     } else {
+//       console.log(trace, 'error');
+//       throw TypeError(`data does not conform to given schema`)
+//     }
+//   }
+// }
+
+module.exports = {
+  trav,
+  validate
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,115 +1,147 @@
 
-const Schema = require('.');
+const {trav, validate} = require('.');
+
+describe('trav', () => {
+
+  describe('identical trav', () => {
+
+    const identicalTrav = (data, schema) => trav(data, schema, ({data}) => data);
+
+    test('positive', () => {  
+      expect(identicalTrav(123, 'whatever schema')).toBe(123);
+      expect(identicalTrav([], 'whatever schema')).toEqual([]);
+      expect(identicalTrav({}, 'whatever schema')).toEqual({});
+      expect(identicalTrav({number:'123'}, 'whatever schema')).toEqual({number: '123'});
+    })
+
+    test('negative', () => {
+      expect(() => identicalTrav(123)).toThrow('Schema should be provided.');
+      expect(() => identicalTrav('asd', [])).toThrow('asd is not array');
+    })
+  })
+
+})
 
 describe('validate', () => {
+  test('prim', () => {
 
-  test('prim data type', () => {
+    const ok = {ok: true};
 
-    const string = new Schema('string')
-    const boolean = new Schema('boolean')
-    const number = new Schema('number')
-
-    // positive
-    expect(string.validate('123')).toEqual({ok:true, type: 'prim'});
-    expect(boolean.validate(true)).toEqual({ok:true, type: 'prim'});
-    expect(number.validate(123)).toEqual({ok:true, type: 'prim'});
-
-    // negative
-    expect(string.validate(123)).toEqual({ok:false, trace:{data: 123, schema:'string'}, type: 'prim'});
-  })
-
-  test('dict of prim', () => {
-
-    const schema = new Schema({
-      name: 'string',
-      age: 'number',
-      isDone:'boolean',
-    })
-
-    const dataPos = {
-      name: 'marvin tau',
-      age: 34,
-      isDone: true
-    }
-
-    const dataPos2 = {
-      name: "newmin, mai",
-      age: 123,
-      isDone: 'true'
-    }
-
-    const res1 = schema.validate(dataPos);
-    const res2 = schema.validate(dataPos2);
-    // expect(ok).toBe(fa);
-    // expect(res.ok).toBe(false);
-
-    console.log(res1, dataPos, schema, 'yeaha ');
-  })
-
-  test('list of prim', () => {
-    const schema = new Schema(['number']);
-
-    const res = schema.validate([123, 345]);
-    expect(res.ok).toBe(true);
-  })
-
-  test ('list of dict', () => {
-
-    const schema = new Schema([{number: 'number'}]);
-    const res2 = schema.validate([
-      {number: 123},
-      {number: 123},
-      {hahaha: 123},
-      {number: 123},
-    ])
-
-    console.log(res2);
-    expect(res2.ok).toBe(false);
-  })
-
-  test('unexpected', () => {
-    expect(() => new Schema()).toThrow('schema cannot be undefined.');
-    expect((new Schema(123)).validate(123)).toEqual({ok: false, trace:{data:123, schema:123}, type: 'unknown'});
+    expect(validate(123, 'number')).toEqual(ok);
+    expect(validate('123', 'string')).toEqual(ok);
+    // expect(validate)
   })
 })
 
-describe('create', () => {
+// describe('validate', () => {
 
-  test('create',() => {
-    const schema = new Schema('string');
-    const data = '123';
-    const res = schema.create(data);
-    expect(res).toBe('123');
-  })
+//   test('prim data type', () => {
 
-  test('create complex', () => {
-    const schema = new Schema({
-      name: 'string'
-    })
+//     const string = new Schema('string')
+//     const boolean = new Schema('boolean')
+//     const number = new Schema('number')
 
-    const data = {
-      name: 'ACCRUAL_ANALYSIS',
-      isCascaded: true,
-      isHidingManual: true,
-      tools: ['ImportExcel', 'SaveRemote', 'ExportExcel'],
-      // referredSheetNames: ['BALANCE'],
-      colSpecs: {
-        ccode_name: {desc: '科目名称', width: 2, isFilterable: true},
-        // iyear: {desc:'会计年', width: 1, isFilerable: true},
-        // iperiod: {desc:'会计月', width: 1, isFilerable: true},
-        // dbill_date: {desc:'记账时间', width: 1, isFilerable: true},
-        // voucher_line_num: {desc:'行号', width: 1, isFilerable: true},
-        md: {desc: '借方发生', width: 1, isFilterable: true, isSortable: true, cellType:'Number'},
-        mc: {desc: '贷方发生', width: 1, isFilterable: true, isSortable: true, cellType:'Number'},
-        dest_ccode_name: {desc: '对方科目', width: 2, isFilterable: true},
-        descendant_num: {desc: '笔数', width: 1, isSortable: true},
-        digest: {desc:'摘要', width: 4, isFilerable: true},
-        // dest_ccode: {desc: '对方编码', width: 1, isFilterable: true},
-        analyzed: {desc:'已分析', width: 1}  
-       }
-    }
+//     // positive
+//     expect(string.validate('123')).toEqual({ok:true, type: 'prim'});
+//     expect(boolean.validate(true)).toEqual({ok:true, type: 'prim'});
+//     expect(number.validate(123)).toEqual({ok:true, type: 'prim'});
 
-    expect(schema.create(data)).toEqual({name:'ACCRUAL_ANALYSIS'});
-  });
+//     // negative
+//     expect(string.validate(123)).toEqual({ok:false, trace:{data: 123, schema:'string'}, type: 'prim'});
+//   })
 
-})
+//   test('dict of prim', () => {
+
+//     const schema = new Schema({
+//       name: 'string',
+//       age: 'number',
+//       isDone:'boolean',
+//     })
+
+//     const dataPos = {
+//       name: 'marvin tau',
+//       age: 34,
+//       isDone: true
+//     }
+
+//     const dataPos2 = {
+//       name: "newmin, mai",
+//       age: 123,
+//       isDone: 'true'
+//     }
+
+//     const res1 = schema.validate(dataPos);
+//     const res2 = schema.validate(dataPos2);
+//     // expect(ok).toBe(fa);
+//     // expect(res.ok).toBe(false);
+
+//     console.log(res1, dataPos, schema, 'yeaha ');
+//   })
+
+//   test('list of prim', () => {
+//     const schema = new Schema(['number']);
+
+//     const res = schema.validate([123, 345]);
+//     expect(res.ok).toBe(true);
+//   })
+
+//   test ('list of dict', () => {
+
+//     const schema = new Schema([{number: 'number'}]);
+//     const res2 = schema.validate([
+//       {number: 123},
+//       {number: 123},
+//       {hahaha: 123},
+//       {number: 123},
+//     ])
+
+//     console.log(res2);
+//     expect(res2.ok).toBe(false);
+//   })
+
+//   test('unexpected', () => {
+//     expect(() => new Schema()).toThrow('schema cannot be undefined.');
+//     expect((new Schema(123)).validate(123)).toEqual({ok: false, trace:{data:123, schema:123}, type: 'unknown'});
+//   })
+// })
+
+// describe('create', () => {
+
+//   test('create',() => {
+//     const schema = new Schema('string');
+//     const data = '123';
+//     const res = schema.create(data);
+//     expect(res).toBe('123');
+//   })
+
+//   test('create complex', () => {
+//     const schema = new Schema({
+//       name: 'string'
+//     })
+
+//     const data = {
+//       name: 'ACCRUAL_ANALYSIS',
+//       isCascaded: true,
+//       isHidingManual: true,
+//       tools: ['ImportExcel', 'SaveRemote', 'ExportExcel'],
+//       // referredSheetNames: ['BALANCE'],
+//       colSpecs: {
+//         ccode_name: {desc: '科目名称', width: 2, isFilterable: true},
+//         // iyear: {desc:'会计年', width: 1, isFilerable: true},
+//         // iperiod: {desc:'会计月', width: 1, isFilerable: true},
+//         // dbill_date: {desc:'记账时间', width: 1, isFilerable: true},
+//         // voucher_line_num: {desc:'行号', width: 1, isFilerable: true},
+//         md: {desc: '借方发生', width: 1, isFilterable: true, isSortable: true, cellType:'Number'},
+//         mc: {desc: '贷方发生', width: 1, isFilterable: true, isSortable: true, cellType:'Number'},
+//         dest_ccode_name: {desc: '对方科目', width: 2, isFilterable: true},
+//         descendant_num: {desc: '笔数', width: 1, isSortable: true},
+//         digest: {desc:'摘要', width: 4, isFilerable: true},
+//         // dest_ccode: {desc: '对方编码', width: 1, isFilterable: true},
+//         analyzed: {desc:'已分析', width: 1}  
+//        }
+//     }
+
+//     expect(schema.create(data)).toEqual({name:'ACCRUAL_ANALYSIS'});
+//   });
+
+// })
