e251c9d20b9ea04700a76dc452e757abed88dad7
marvintau <nightvista@gmail.com>
2020-05-29 10:13:41 +0800

    fix: dadd test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,18 @@
-const Schema = require('.');
+const Schema = require('../src');
+const {create} = require('../src/create');
+const {validate} = require('../src/validate');
 
 describe('create schema class', () => {
   test('create schema instance', () => {
     expect(() => new Schema()).toThrow('Schema cannot be undefined.')
+  })
+
+  test('create instanace', () => {
+    const data = [123, 456];
+    const schemaData = ['number'];
+
+    const schema = new Schema(schemaData);
+
+    expect(schema.create(data)).toBe(create(data, schemaData));
   })
 })
